006f37e2ba6d1cacba19fcbdb464fd7e50be223d
KoheiNegishi <nk.0910.shanti@gmail.com>
2024-04-25 14:37:38 +0900

    fix: 会員登録ページの編集

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -10,15 +10,12 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     </head>
-
     <header class="header">
         <div class="header__inner">
             <div class="header__logo">Atte</div>
         </div>
     </header>
-
     <main class="sm:justify-center">
-
         <h2 class="">会員登録</h2>
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
@@ -28,9 +25,6 @@
             <div>
                 <x-label for="name" :value="__('')" />
                 <x-input id="name" class="form-content" type="text" name="name" placeholder="名前" :value="old('name')" required autofocus />
-                @error('name')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
             </div>
             <!-- Email Address -->
             <div class="mt-4">
@@ -54,13 +48,11 @@
                                 type="password"
                                 name="password_confirmation" required />
             </div>
-
             <div class="mt-4 form-container">
                 <x-button class="form-button">
                     {{ __('会員登録') }}
                 </x-button>
             </div>
-
             <div class="mt-4 login" >
                 <div class="login-text">アカウントをお持ちの方はこちら<br/></div>
                 <a class="login-link" href="{{ route('login') }}">
@@ -69,11 +61,7 @@
             </div>
         </form>
     </main>
-
     <footer class="footer">
         <small>Atte, Inc.</small>
     </footer>
-
 </x-guest-layout>
-
-
